b370b1e6ab7393e1f2f093dd03f1aea854d46ddd
Lance Kellar <ritterstandalpha@gmail.com>
2023-12-23 21:15:30 -0500

    Remove tk

--- Youtuber.py
+++ Youtuber.py
@@ -9,13 +9,14 @@ from elevenlabs import generate, set_api_key
 import os
 import subprocess
 from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
-from tkinter import Tk
-from tkinter.filedialog import askdirectory
+import moviepy.editor as me
+import re
 
 st.title("Youtube Saver")
 
 link = st.text_input("Link to Youtube Video", key="link")
 
+to_mp3 = st.sidebar.checkbox('to_mp3')
     
 
 if st.button("Download!"):
@@ -26,7 +27,11 @@ if st.button("Download!"):
 
     if yt is not None:
         st.subheader(yt.title)
+        temp_name = re.sub('[^A-Za-z0-9\s]+', '', yt.title)
         st.image(yt.thumbnail_url)
         audio_name = st.caption("Streaming from the cloud to your silicon empire...")
         streams = yt.streams.filter()
         filename = streams.first().download(output_path=folder_path)
+        if to_mp3:
+            audio = me.AudioFileClip(filename)
+            audio.write_audiofile(f"{temp_name}.mp3")
